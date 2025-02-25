e79813b3a161746b1dfdf4235f0e5f6ba4ddfef2
anand91m <anand91m@gmail.com>
2025-02-26 00:24:18 +0530

    create test runner and intergrate in testng xml

---
+++ src/test/java/com/qa/saucedemo/testrunner/TestRunner.java
@@ -0,0 +1,12 @@
+package com.qa.saucedemo.testrunner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = {}, glue = { "com.qa.saucedemo.stepdefinitions" }, // Step definition package
+		plugin = { "pretty", "html:target/cucumber-reports.html"
+		}, monochrome = true)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+}
+
